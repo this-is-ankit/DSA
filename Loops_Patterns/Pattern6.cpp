8d4a1fb86d24f1f1ad308547bafb4dd0812abe78
Ankit Kumar <ankitmayank829@gmail.com>
2026-07-14 15:17:29 +0530

    sixth pattern solved

---
+++ Loops_Patterns/Pattern6.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main() {
+    int i , j , n ;
+    scanf("%d" , &n);
+    for(i = n ; i > 0 ; i--){
+        for(j= 1 ; j<=i; j++){
+            printf("%d" ,j);
+        }
+        printf("\n");
+    }
+}
