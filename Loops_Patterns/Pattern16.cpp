6e564f2255e8cfeff376d7600caac66c0311b00e
Ankit Kumar <ankitmayank829@gmail.com>
2026-07-14 18:54:37 +0530

    16th pattern solved

---
+++ Loops_Patterns/Pattern16.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int i,j,n;
+    char c,k='A';
+    cin >> n;
+    for(i = 1 ;i<=n;i++){
+       
+        for(c = 'A';c <= k;c++){
+            cout << k;
+            
+        }
+         k++;
+        cout << endl;
+    }
+}
