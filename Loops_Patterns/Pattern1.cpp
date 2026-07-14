00d26b90846c83411aba761388810af16d90531d
Ankit Kumar <ankitmayank829@gmail.com>
2026-07-14 14:59:41 +0530

    first pattern solved

--- Loops_Patterns/Pattern1.cpp
+++ Loops_Patterns/Pattern1.cpp
@@ -1,5 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std ;
 int main () {
-    
+    int i , j , n;
+    scanf("%d" , &n);
+    for (i =0 ; i<n ; i++){
+        for(j= 0 ; j<n ; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
 }
