db603369aa2847cb69de51310507702177e10076
Ankit Kumar <ankitmayank829@gmail.com>
2026-07-15 18:18:05 +0530

    20th pattern solved

---
+++ Loops_Patterns/Pattern20.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i, j, n;
+    cin >> n;
+     for (i = 1; i <=n; i++)
+    {
+        for (j = i; j >=1; j--)
+        {
+            cout << "*";
+        }
+        for (j = 1; j <= 2 * (n - i) ; j++)
+        {
+            cout << " ";
+        }
+       for (j = i; j >=1; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (i = n-1; i >= 1; i--)
+    {
+        for (j = i; j >=1; j--)
+        {
+            cout << "*";
+        }
+        for (j = 1; j <= 2 * (n - i) ; j++)
+        {
+            cout << " ";
+        }
+       for (j = i; j >=1; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+   
+}
