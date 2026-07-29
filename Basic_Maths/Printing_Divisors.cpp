5bb981a2d999315a81dc9e4c716d8c79c4e6c283
Ankit Kumar <ankitmayank829@gmail.com>
2026-07-29 20:19:29 +0530

    Divisors printing using more optimal way

--- Basic_Maths/Printing_Divisors.cpp
+++ Basic_Maths/Printing_Divisors.cpp
@@ -1,11 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int n;
-    cin >>n;
-    for(int i =1;i*i<=n;i++){
-        if(n%i == 0) {cout << i << endl;
-            if(n/i != i) cout << n/i << endl;
+    cin >> n;
+    vector<int> v;
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            v.push_back(i);
+            if (n / i != i)
+                v.push_back(n/i);
         }
+    }
+    sort(v.begin() , v.end());
+    for(auto it : v){
+        cout << it << " ";
     }
 }
