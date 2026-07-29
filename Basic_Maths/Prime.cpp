e9617c260de1b64873bc94a80ffc744593dda039
Ankit Kumar <ankitmayank829@gmail.com>
2026-07-29 20:58:02 +0530

    Prime numbers done in optimal way

--- Basic_Maths/Prime.cpp
+++ Basic_Maths/Prime.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int main() {
     int n ,count =0;
     cin >> n;
-    for(int i=1;i<=sqrt(n)/2;i++){
+    for(int i=1;i*i<=n;i++){
         if(n%i == 0) count++;
+        if(n/i != i && n % (n/i) == 0) count++;
     }
-    if(count!=2) cout << "The number is not a prime number";
-    else cout << "The number is  a prime a number";
+    if(count==2) cout << "The number is  a prime number";
+    else cout << "The number is not a prime a number";
     return 0;
 }
