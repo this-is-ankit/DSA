3b85d005dc5670cb1ef79e9a7395367178487811
Ankit Kumar <ankitmayank829@gmail.com>
2026-07-30 16:59:26 +0530

    Problem 8 done in the most optimal way

--- Recursions/08_Palindrome.cpp
+++ Recursions/08_Palindrome.cpp
@@ -1,22 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool palindrome(string s)
-{
-    if (s.size() <= 1) return true;
+bool isPalindrome(string s)
+{   // This solution fails in leetcode because it runs a very massive testcase and the recursive call and the substr funcion causes the lack of memory
+    // When i ran the task it said Memory limit exceeded, it is because the sustr on every call created a new copy of the string from the indices it takes as argument
+    // Now for a very massive test case, it will break the memory 
+    // Therefore, in the 09 file I did this with non recursive way
+        
+        if (s.length() <= 1) return true;
 
-    if (s.front() != s.back())
-    {
-        return false;
+        
+        if (!isalnum(s.front())) {
+            return isPalindrome(s.substr(1)); 
+        }
+
+        
+        if (!isalnum(s.back())) {
+            return isPalindrome(s.substr(0, s.length() - 1));
+        }
+
+        if (tolower(s.front()) != tolower(s.back())) {
+            return false;
+        }
+        return isPalindrome(s.substr(1, s.length() - 2));
     }
-
-    return palindrome(s.substr(1,s.size()-2));
-}
 int main()
 {
     string s;
     cin >> s;
-    // cout<< s.length();
-    // cout<< s.size();
-    cout << palindrome(s);
+    // cout << s.front();
+    // cout << s.back();
+    // cout<< s.length() << endl;
+    // cout<< s.size() << endl;
+    cout << isPalindrome(s);
     return 0;
 }
