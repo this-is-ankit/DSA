f4a4f9d89cf5d9b6dff8e39abe72510cf789f97e
Ankit Kumar <ankitmayank829@gmail.com>
2026-07-30 17:29:43 +0530

    Problem 9 done

---
+++ Recursions/09_Palindrome.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPalindrome(string s){
+    int left = 0 , right = s.length() -1;
+    while(left < right){
+        if(!isalnum(s[left])) left++;
+        else if(!isalnum(s[right])) right--;
+        else if (tolower(s[left]) != tolower(s[right])) return false;
+        else{
+            left++;
+            right--;
+        }
+    }
+    return true;
+}
+int main(){
+    string s;
+    cin >>s;
+    cout << isPalindrome(s);
+}
