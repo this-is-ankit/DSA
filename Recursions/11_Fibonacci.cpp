78b7373b786a4cf1630c21f6991e1d421558a0b7
Ankit Kumar <ankitmayank829@gmail.com>
2026-07-30 18:54:34 +0530

    Problem 11 done

---
+++ Recursions/11_Fibonacci.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fib(int n){
+    if(n<=0) return 0;
+    else if(n == 1) return 1;
+    int a=0, b = 1 , sum = 0;
+    for(int i =2; i<= n; i++){
+        sum=a+b;
+        a =b;
+        b = sum;
+    }
+    return sum;
+}
+int main(){
+    int n;
+    cin  >> n;
+    cout  << fib(n);
+}
