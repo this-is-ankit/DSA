83ad0c1617db3dd68a38323ab456f5670e93b52b
Ankit Kumar <ankitmayank829@gmail.com>
2026-07-30 15:33:33 +0530

    Problem 7 done agian

--- Recursions/07_Reverse_array.cpp
+++ Recursions/07_Reverse_array.cpp
@@ -1,27 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void reverse(int arr[],int i,int n){
-    if(i >= n/2){
+void reverse(int arr[], int i, int n)
+{
+    if (i >= n / 2)
+    {
         return;
     }
-    int temp = arr[i];
-    arr[i] = arr[n-i-1];
-    arr[n-i-1] = temp;
-    reverse(arr,i+1,n);
+    swap(arr[i],arr[n - i - 1]);
+    // int temp = arr[i];
+    // C = arr[n - i - 1];
+    // arr[n - i - 1] = temp;
+    reverse(arr, i + 1, n);
 }
-int main(){
+int main()
+{
     int n;
     cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
-    reverse(arr,0,n);
-    for(int i=0;i<n;i++){
+    reverse(arr, 0, n);
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
     return 0;
-
 }
