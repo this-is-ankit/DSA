e005e418f326314976c6a1f0a3d137b0fad89b93
Ankit Kumar <ankitmayank829@gmail.com>
2026-07-30 11:43:06 +0530

    Problem 5 solved

---
+++ 05_Sum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int s = 0;
+int sum(int n)
+{
+    if (n == 0)
+    {
+        return s;
+    }
+    s += n;
+    return sum(n - 1);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout  << sum(n);
+
+}
