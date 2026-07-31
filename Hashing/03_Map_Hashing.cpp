08d54e66b825d7c1b5639d1f5769488a17a72a4e
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-01 01:42:48 +0530

    Map hashing done 03

--- Hashing/03_Map_Hashing.cpp
+++ Hashing/03_Map_Hashing.cpp
@@ -2,21 +2,25 @@
 using namespace std;
 int main() {
     int n;
+    cout << "Enter the number of elements of array : ";
     cin  >> n;
     int arr[n];
     map<int,int> mp;
+    cout << "Enter each elements of array : ";
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
         mp[arr[i]]++;
     }
     int q;
+    cout << "Enter the total number of queries : ";
     cin >> q;
+    cout << "Enter the query one by one : ";
     while (q--)
     {   
         int number;
         cin >> number;
-        cout << mp[number] << endl;
+        cout<< number << " appears " << mp[number] << " times" << endl;
     }
     return 0;
 }
