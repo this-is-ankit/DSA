f747d415c2a94dd5dc2f7c52c8ef02e4a7ff5bd0
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-01 01:35:38 +0530

    Map hashing

---
+++ Hashing/03_Map_Hashing.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin  >> n;
+    int arr[n];
+    map<int,int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[n];
+        mp[arr[i]]++;
+    }
+    int q;
+    cin >> q;
+    while (q--)
+    {   
+        int number;
+        cin >> number;
+        cout << mp[number] << endl;
+    }
+    return 0;
+    
+}
