1203fbe84eb32fb92c9bc07aa55cce90d555f05d
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-01 22:35:04 +0530

    Bubble sorting done

---
+++ Sorting/02_BubbleSort.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+void BubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            if (arr[j + 1] < arr[j])
+                swap(arr[j + 1], arr[j]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    BubbleSort(arr,n);
+    return 0;
+}
