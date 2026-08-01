00d9bf6a8a12bb40f981fcb04fb09f72fe0cce7c
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-01 22:29:12 +0530

    Selection sorting done

--- Sorting/01_Selection_Sort.cpp
+++ Sorting/01_Selection_Sort.cpp
@@ -1,6 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+void selectionSort(int arr[] ,int n){
+    for (int i = 0; i < n-2; i++)
+    {   int min = i;
+        for (int j = i; j < n-1; j++)
+        {
+            if(arr[j] < arr[min]) min = j;
+        }
+        swap(arr[min],arr[i]);
+    }
+}
 int main(){
-    cout << "Hello world";
+   int n;
+   cout << "Enter the size of the array : ";
+   cin >> n;
+   int arr[n];
+   for (int i = 0; i < n; i++)
+   {
+    cin >> arr[i] ;
+   }
+   selectionSort(arr , n);
+   for (int i = 0; i < n; i++)
+   {
+    cout << arr[i] << " " ;
+   }
+   
     return 0;
 }
