f90bdc9525b7cbe89561456a02ce7f532d3b4668
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-02 18:36:05 +0530

    Insertion sort done

---
+++ Sorting/03_InsertionSort.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+void InsetionSort(int arr[] , int n){
+    for (int i = 0; i < n; i++)
+    {
+        int j = i;
+        while (j>0 && arr[j-1] > arr[j])
+        {
+            swap(arr[j] ,arr[j-1] );
+        }
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    InsetionSort(arr,n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] <<  " ";
+    }
+    return 0;
+}
