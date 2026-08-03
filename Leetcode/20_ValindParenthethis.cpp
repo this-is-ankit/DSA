b2bc5f4d4cb539a80f6d19db2036b0519479a3a6
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-03 23:21:55 +0530

    Leetcode question 20 solved

--- Leetcode/20_ValindParenthethis.cpp
+++ Leetcode/20_ValindParenthethis.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 bool isValid(string s)
 {
-    int n = s.size(), j;
+    int n = s.size();
     stack<char> st;
     for (int i = 0; i < n; i++)
     {
-        j = i;
-        if (s[j] == '(' || '{' || '[')
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
         {
-            st.push(s[j]);
+            st.push(s[i]);
         }
         else
         {
+            if (st.empty())
+                return false;
             char top = st.top();
             st.pop();
-            if ((s[j] == ')' && top != '(') || (s[j] == '}' && top != '{') || (s[j] == ']' && top != '['))
+            if ((s[i] == ')' && top != '(') || (s[i] == '}' && top != '{') || (s[i] == ']' && top != '['))
                 return false;
         }
     }
