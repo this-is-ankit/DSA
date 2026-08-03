7fe5b2ce4c977b0229acd949b8cb5ebf90ebfa9e
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-03 20:31:09 +0530

    Quicksort

---
+++ Sorting/05_QuickSort.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+int Partition(vector<int> &v, int low, int high)
+{
+    int i = low, j = high;
+    while (low < high)
+    {
+        int pivot = v[0];
+
+        while (v[i] >= pivot)
+        {
+            i++;
+        }
+        while (v[j] < pivot)
+        {
+            j++;
+        }
+        swap(v[i], v[j]);
+    }
+    return j;
+}
+void Quicksort(vector<int> &v, int low, int high)
+{
+    if (low < high)
+        return;
+    int pivot = Partition(v, low, high);
+    Quicksort(v, low, pivot);
+    Quicksort(v, pivot + 1, high);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int j;
+        cin >> j;
+        v.push_back(j);
+    }
+    Quicksort(v,0,n-1);
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    
+}
