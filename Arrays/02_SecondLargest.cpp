c9d0a582ca64b57f9b8e92b7ad8f30bb43f34f88
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-04 20:34:18 +0530

    Arrays 02 problem done

---
+++ Arrays/02_SecondLargest.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int Largest(int arr[] , int n){
+    int l = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i] > l) l = arr[i];
+    }
+return l;
+}
+int main() {
+    int n;
+    cout << "Enter the number of the elements : ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements one by one : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "The largest element in the given array is : " << Largest(arr,n);
+    return 0;
+}
