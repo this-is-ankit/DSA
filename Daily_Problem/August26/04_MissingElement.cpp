fbba4b1b75ce68df62c328660c1dee3dc3457b7a
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-04 16:46:18 +0530

    Daily Problem 04 done

---
+++ Daily_Problem/August26/04_MissingElement.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findMissingElements(vector<int> &nums)
+{
+    int n = nums.size();
+    int s = nums[0], l = nums[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] < s)
+            s = nums[i];
+        else if (nums[i] > l)
+            l = nums[i];
+    }
+    vector<int> temp;
+    while (s != l)
+    {
+        s++;
+        if (s == l)
+            return temp;
+        int found = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (nums[j] == s)
+            {
+                found++;
+            }
+        }
+        if (found == 0)
+        {
+            temp.push_back(s);
+        }
+    }
+    return temp;
+}
+int main(){
+    vector<int> arr = {7,8,6,9};
+    vector<int> temp  = findMissingElements(arr);
+    for(auto it : temp){
+        cout << it << " ";
+    }
+    return 0;
+}
