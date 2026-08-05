f5eb8688c52a91878c6ce3087daef4b9a08a15ac
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-05 22:55:24 +0530

    Leetcode done

---
+++ Leetcode/13_Roman_to_Integer.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int romanToInt(string s)
+{
+    unordered_map<char, int> mp = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}};
+
+    int ans = 0;
+    int n = s.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i < n - 1 && mp[s[i]] < mp[s[i + 1]])
+        {
+            ans -= mp[s[i]];
+        }
+        else
+        {
+            ans += mp[s[i]];
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    string s = "LVIII";
+    cout << romanToInt(s);
+    return 0;
+}
