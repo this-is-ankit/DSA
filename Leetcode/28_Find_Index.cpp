262187f9a70f255ac3e174c19216bc538fd781b6
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-08 22:56:31 +0530

    Problem 28..

--- Leetcode/28_Find_Index.cpp
+++ Leetcode/28_Find_Index.cpp
@@ -1,5 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
+int strStr(string haystack, string needle) {
+        int pos = haystack.find(needle);
+        return pos != string::npos ? pos : -1;
+    }
 int main(){
+
     return 0;
 }
