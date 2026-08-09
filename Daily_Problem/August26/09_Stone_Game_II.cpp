451671c37db3067abb4d23d4c6db3bc7ccd6178e
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-09 18:40:52 +0530

    Daily problem initated.

--- Daily_Problem/August26/09_Stone_Game_II.cpp
+++ Daily_Problem/August26/09_Stone_Game_II.cpp
@@ -1,5 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+int dp[101][101];
+    int suf[101];
+    int n;
+int solve(int i, int m) {
+        if (i >= n) return 0;
+        if (i + 2 * m >= n) return suf[i];
+        if (dp[i][m] != -1) return dp[i][m];
+
+        int ans = 0;
+        for (int x = 1; x <= 2 * m; x++) {
+            ans = max(ans, suf[i] - solve(i + x, max(m, x)));
+        }
+        return dp[i][m] = ans;
+    }
 int main(){
     return 0;
 }
