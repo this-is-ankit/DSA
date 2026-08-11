c11e946294286b5b69d855dd9cb52d5b8d5e210e
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-11 23:53:21 +0530

    Solved completely

--- Leetcode/4_Median_of_Two_Sorted_Arrays.cpp
+++ Leetcode/4_Median_of_Two_Sorted_Arrays.cpp
@@ -72,6 +72,9 @@ double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
     return 0.0;
 }
 int main()
-{
+{   
+    vector<int> nums1 = {1,2};
+    vector<int> nums2 = {3,4};
+    cout<<findMedianSortedArrays(nums1,nums2);
     return 0;
 }
