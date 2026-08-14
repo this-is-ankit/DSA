0529635b7a7efbb7a53078155656031877eff139
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-14 23:03:47 +0530

    Function works

--- Daily_Problem/August26/14_Maximum_Length_Substring.cpp
+++ Daily_Problem/August26/14_Maximum_Length_Substring.cpp
@@ -1,5 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+int maximumLengthSubstring(string s) {
+        vector<int> cnt(26, 0);
+        int l = 0, ans = 0;
+        
+        for (int r = 0; r < s.size(); r++) {
+            cnt[s[r] - 'a']++;
+            while (cnt[s[r] - 'a'] > 2) {
+                cnt[s[l] - 'a']--;
+                l++;
+            }
+            ans = max(ans, r - l + 1);
+        }
+        
+        return ans;
+    }
 int main(){
     return 0;
 }
