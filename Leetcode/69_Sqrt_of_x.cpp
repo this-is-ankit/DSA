90b93b14373af97389bfe8184daacfab98f3f811
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-14 23:07:54 +0530

    Trying leetcode problem 69.

--- Leetcode/69_Sqrt_of_x.cpp
+++ Leetcode/69_Sqrt_of_x.cpp
@@ -1,5 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int mySqrt(int x)
+{
+    if (x == 0)
+        return 0;
+    int l = 1, r = x, ans = 0;
+    while (l <= r)
+    {
+        long long m = l + (r - l) / 2;
+        if (m * m <= x)
+        {
+            ans = m;
+            l = m + 1;
+        }
+        else
+        {
+            r = m - 1;
+        }
+    }
+    return ans;
+}
+int main()
+{
     return 0;
 }
