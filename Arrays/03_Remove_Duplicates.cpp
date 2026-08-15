12c627ec450169740b5cb662ffa201fe8348d896
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-15 17:31:57 +0530

    Array problem 4 from a2z sheet almost done

--- Arrays/03_Remove_Duplicates.cpp
+++ Arrays/03_Remove_Duplicates.cpp
@@ -1,5 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+int removeDuplicates(vector<int> &nums){
+    if(nums.empty()) return 0;
+    int n=nums.size();
+    int i = 0;
+    for (int j = 0; j < n; j++)
+    {
+        if(nums[i]!=nums[j]){
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i+1;
+     
+}
 int main(){
     return 0;
 }
