87bdbab009af1e760842aaaca5c7c0572077a760
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-15 17:51:37 +0530

    Array problem 5 from a2z sheet done

--- Arrays/05_Left_Rotate.cpp
+++ Arrays/05_Left_Rotate.cpp
@@ -1,5 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+void rotateleft(vector<int> &nums , int k){
+    int n = nums.size();
+    k%=n;
+    reverse(nums[0], nums[k]);
+    reverse(nums[k] , nums[n]);
+    reverse(nums.begin() , nums.end());
+}
 int main(){
     return 0;
+    vector<int>nums = {1,3,4,5,8,9,6};
+    rotateleft(nums,3);
+    for(auto it : nums){
+        cout<< it << " ";
+    }
+return 0;
+    
 }
