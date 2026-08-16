4a41e3852c6cb1c29188700f51dea68864391c9c
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-16 13:23:14 +0530

    Array problem 8 from a2z sheet done omtimal approach.

--- Arrays/08_Intersection.cpp
+++ Arrays/08_Intersection.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> Union(vector<int> &a, vector<int> &b)
+vector<int> Intersection(vector<int> &a, vector<int> &b)
 {
     int n1 = a.size(), n2 = b.size();
     int i = 0, j = 0;
@@ -28,7 +28,7 @@ int main()
 {
     vector<int> a = {1, 2, 2, 3, 3, 4, 5, 6};
     vector<int> b = {2, 3, 3, 5, 6, 6, 7};
-    vector<int> temp = Union(a, b);
+    vector<int> temp = Intersection(a, b);
     for (auto it : temp)
     {
         cout << it << " ";
