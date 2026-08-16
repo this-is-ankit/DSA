a89e29940127bc4373c613c88b931a5f7eab36d4
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-16 12:27:08 +0530

    Array problem 7 from a2z sheet optimal solution done

--- Arrays/07_Union_of_Sorted_arrays.cpp
+++ Arrays/07_Union_of_Sorted_arrays.cpp
@@ -7,28 +7,41 @@ vector<int> Union(vector<int> &a, vector<int> &b)
     vector<int> temp;
     while (i < n1 && j < n2)
     {
-        if (a[i] < b[j])
+        if (a[i] <= b[j])
         {
-            temp.push_back(a[i]);
+            if (temp.size() == 0 || a[i] != temp.back())
+                temp.push_back(a[i]);
             i++;
         }
-        else if (a[i] > b[j])
+        else
         {
-            temp.push_back(b[j]);
+            if (temp.size() == 0 || b[j] != temp.back())
+                temp.push_back(b[j]);
             j++;
         }
     }
     while (i < n1)
     {
-        temp.push_back(a[i]);
+        if (temp.size() == 0 || a[i] != temp.back())
+            temp.push_back(a[i]);
+        i++;
     }
     while (j < n2)
     {
-        temp.push_back(b[j]);
+        if (temp.size() == 0 || b[j] != temp.back())
+            temp.push_back(b[j]);
+        j++;
     }
     return temp;
 }
 int main()
 {
+    vector<int> a = {1, 2, 5, 6, 7, 8};
+    vector<int> b = {1, 2, 3, 5, 8, 9, 11};
+    vector<int> temp = Union(a, b);
+    for (auto it : temp)
+    {
+        cout << it << " ";
+    }
     return 0;
 }
