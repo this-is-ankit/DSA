a497643ae1d0ee4ce6e37c9bd2626b491b218f50
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-16 14:33:45 +0530

    Array problem 10 from a2z sheet done omtimal approach.

---
+++ Arrays/10_Max_Consecutinve_Ones.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int findMaxConsecutiveOnes(vector<int> &nums)
+{
+    int count = 0, temp = -1;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == 1)
+        {
+            count++;
+        }
+        else
+        {
+            if (count >= temp)
+            {
+                temp = count;
+                count = 0;
+            }
+        }
+    }
+    return temp;
+}
+int main()
+{
+    vector<int> nums = {1, 0, 1, 1, 0, 1};
+    cout << findMaxConsecutiveOnes(nums);
+    return 0;
+}
