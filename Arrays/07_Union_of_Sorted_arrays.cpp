baa71f8e56a78b1e596d3e86179cfc47052c79d2
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-16 12:14:13 +0530

    Array problem 7 from a2z sheet done

--- Arrays/07_Union_of_Sorted_arrays.cpp
+++ Arrays/07_Union_of_Sorted_arrays.cpp
@@ -1,5 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+vector<int> Union(vector<int> &a, vector<int> &b)
+{
+    int n1 = a.size(), n2 = b.size();
+    int i = 0, j = 0;
+    vector<int> temp;
+    while (i < n1 && j < n2)
+    {
+        if (a[i] < b[j])
+        {
+            temp.push_back(a[i]);
+            i++;
+        }
+        else if (a[i] > b[j])
+        {
+            temp.push_back(b[j]);
+            j++;
+        }
+    }
+    while (i < n1)
+    {
+        temp.push_back(a[i]);
+    }
+    while (j < n2)
+    {
+        temp.push_back(b[j]);
+    }
+    return temp;
+}
+int main()
+{
     return 0;
 }
