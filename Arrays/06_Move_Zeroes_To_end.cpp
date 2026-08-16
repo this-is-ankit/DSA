7ee3567a9999e023bdb7d1969c511caf310ac9e4
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-16 10:25:41 +0530

    Array problem 6 from a2z sheet optmized approach.

--- Arrays/06_Move_Zeroes_To_end.cpp
+++ Arrays/06_Move_Zeroes_To_end.cpp
@@ -1,6 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-
+void moveZeroes(vector<int> &nums)
+{
+    if (nums.empty())
+        return;
+    int n = nums.size();
+    int i = -1;
+    for (int j = 0; j < n; j++)
+    {
+        if (nums[j] != 0)
+        {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    i++;
+    while (i < n)
+    {
+        nums[i] = 0;
+    }
+}
+int main()
+{
 }
