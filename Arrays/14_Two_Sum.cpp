538a7b979443f96b8328ba123d4c0ee08f0fbc57
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-17 23:55:55 +0530

    Problem 14 from a2z sheet done

--- Arrays/14_Two_Sum.cpp
+++ Arrays/14_Two_Sum.cpp
@@ -1,6 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(){
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    unordered_map<int, int> seen;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int complement = target - nums[i];
+        if (seen.count(complement))
+        {
+            return {seen[complement], i};
+        }
+        seen[nums[i]] = i;
+    }
+    return {};
+}
+int main()
+{
     return 0;
 }
