9d8b1c1d268d3d4eb590f58d954647aeff143448
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-17 23:57:40 +0530

    Problem 14 from a2z sheet done.

--- Arrays/14_Two_Sum.cpp
+++ Arrays/14_Two_Sum.cpp
@@ -17,7 +17,7 @@ vector<int> twoSum(vector<int> &nums, int target)
 int main()
 {
     vector<int> nums = {2, 7, 11, 15};
-    vector<int> temp = twoSum(nums);
+    vector<int> temp = twoSum(nums,9);
     for (auto it : temp)
     {
         cout << it << " ";
