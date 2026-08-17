5e2090809555d659bdec6108ee6ef111b5275ef2
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-17 23:53:19 +0530

    Done with the problem  number 15 from a2z sheet optimalyy

--- Arrays/15_Sort_colors.cpp
+++ Arrays/15_Sort_colors.cpp
@@ -2,33 +2,33 @@
 using namespace std;
 void sortColors(vector<int> &nums)
 {
-    int count0 = 0, count1 = 0, count2 = 0;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if (nums[i] == 0)
-            count0++;
-        if (nums[i] == 1)
-            count1++;
-        if (nums[i] == 2)
-            count2++;
-    }
-    int i = 0, j = (count0 + count1) - count0, k = (count0 + count1 + count2) - count0;
+    int low = 0, mid = 0, high = nums.size() - 1;
 
-    while (i < count0 || j < count0 + count1 || k < count0 + count1 + count2)
+    while (mid <= high)
     {
-        nums[i]=0;
-        i++;
-        nums[j] =1;
-        j++;
-        nums[k]=2;
-        k++;
+        if (nums[mid] == 0)
+        {
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            swap(nums[mid], nums[high]);
+            high--;
+        }
     }
 }
 int main()
 {
-    vector<int> nums = {0,1,2,0,1,2,1,2,0};
+    vector<int> nums = {0, 1, 2, 0, 1, 2, 1, 2, 0};
     sortColors(nums);
-    for(auto it : nums){
+    for (auto it : nums)
+    {
         cout << it << " ";
     }
     return 0;
