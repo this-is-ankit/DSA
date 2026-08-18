e6fbd1e385702bbd5919ba1d806b9eae4990377a
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-18 22:15:31 +0530

    Problem 16 from a2z sheet main logic done

--- Arrays/16_Majority_Element.cpp
+++ Arrays/16_Majority_Element.cpp
@@ -1,2 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+int majorityElement(vector<int>& nums) {
+        int candidate = nums[0];
+        int count = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return candidate;
+    }
+int main(){
+
+}
