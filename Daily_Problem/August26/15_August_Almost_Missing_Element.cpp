41e45c04708c36a2cec928d30b2377ecd7f46716
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-19 22:12:23 +0530

    Daily problem 15 august almost done.

--- Daily_Problem/August26/15_August_Almost_Missing_Element.cpp
+++ Daily_Problem/August26/15_August_Almost_Missing_Element.cpp
@@ -1,6 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-int largestInteger(vector<int> &nums,k){
+int largestInteger(vector<int> &nums,int k){
+    if(nums.empty()) return 0;
+    if(k> nums.size()) return;
+    int i=0;
+    int hash[INT_MAX] = {0};
+    while (i<=nums.size() - k)     
+    {
+        for (int j = i; j < k; j++)
+        {
+            
+           hash[nums[j]]++;
+        }
+        i++;
+    }
+    for(auto num : hash){
+     return (num==1);
+    }
     
 }
 int main(){
