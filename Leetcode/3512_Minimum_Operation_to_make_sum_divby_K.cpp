5de2c76f7e045229b661ad0ba6516c811f73246a
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-20 23:29:45 +0530

    Leetcode problem 3512 almost done

--- Leetcode/3512_Minimum_Operation_to_make_sum_divby_K.cpp
+++ Leetcode/3512_Minimum_Operation_to_make_sum_divby_K.cpp
@@ -1,5 +1,15 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int minOperations(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += nums[i];
+    return sum%k;
+}
+int main()
+{
+
     return 0;
 }
