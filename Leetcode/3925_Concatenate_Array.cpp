9a7b27a6dcbf311360c3b4e8808afb5f41cb6122
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-20 23:42:28 +0530

    Leetcode problem 3925 done

--- Leetcode/3925_Concatenate_Array.cpp
+++ Leetcode/3925_Concatenate_Array.cpp
@@ -1,6 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(){
+vector<int> concatWithReverse(vector<int> &nums)
+{
+    vector<int> ans;
+    int n = nums.size();
+    for (int i : nums)
+        ans.push_back(i);
+    for (int i = n - 1; i >= 0; i--)
+        ans.push_back(nums[i]);
+    return ans;
+}
+int main()
+{
     return 0;
 }
