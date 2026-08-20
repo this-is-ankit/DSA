64fbfa25a1ab4e8310bff322891e8516f5606a23
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-20 23:04:56 +0530

    Daily problem 20 august done

--- Daily_Problem/August26/20_August_Distrubute_Element.cpp
+++ Daily_Problem/August26/20_August_Distrubute_Element.cpp
@@ -1,5 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+  vector<int> resultArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> arr1, arr2;
+        if (n == 0 || n == 1)
+            return nums;
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+        for (int i = 2; i < n; i++) {
+            if (arr1.back() > arr2.back()) {
+                arr1.push_back(nums[i]);
+            } else {
+                arr2.push_back(nums[i]);
+            }
+        }
+       arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+        return arr1;
+    }
+int main()
+{
     return 0;
 }
