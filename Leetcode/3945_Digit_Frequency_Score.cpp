90c52eb154dc72faed67167a7f87b5df6b15733e
Ankit Kumar <ankitmayank829@gmail.com>
2026-08-22 01:01:23 +0530

    Leetcode problem 3945 done optimally

--- Leetcode/3945_Digit_Frequency_Score.cpp
+++ Leetcode/3945_Digit_Frequency_Score.cpp
@@ -1,21 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int digitFrequencyScore(int n) {
-    map<int,int> mp;
-    while (n>0)
+int digitFrequencyScore(int n)
+{
+    map<int, int> mp;
+    while (n > 0)
     {
-        mp[n%10]++;
-        n/=10;
+        mp[n % 10]++;
+        n /= 10;
     }
     int k = mp.size();
-    int sum=0;
-    for (auto it : mp) {
-        sum+= it.first*it.second;
-}
-    
+    int sum = 0;
+    for (auto it : mp)
+        sum += it.first * it.second;
+
     return sum;
-    }
-int main() {
+}
+int main()
+{
 
     return 0;
 }
